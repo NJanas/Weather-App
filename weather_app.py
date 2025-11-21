03fc92bbf7ce016ed9114d6cd52d9fa90d43cb83
Njanas <janasntl@gmail.com>
2025-11-21 23:30:50 +0100

    mvp version

--- weather_app.py
+++ weather_app.py
@@ -1,20 +1,33 @@
-from agents import Agent, Runner
+import asyncio
+from agents import Agent, Runner, function_tool
 from dotenv import load_dotenv
 
 load_dotenv() #laduje plik .env (tam gdzie jest klucz)
 
+
+#tool do pozyskiwania informacji pogodowych:
+@function_tool
+def get_weather_information(city:str) -> dict:
+    """Returns weather information from given location"""
+    return {"city":city, "temperature": 32, "temperature unit":"C", "humidity":60}
+
 # Tworzymy agenta:
 agent = Agent(
     name="Weather App", #nazwa agenta
-    instructions="You are a weather app", #system prompt
+    instructions=(
+        "You are a weather app.\n"
+        "Use tools to get weather information.\n"
+        "Answer in 1-2 sentence with rhymes."
+    ), #system prompt
     model="gpt-5-nano", #podajemy nazwę modelu który chcemy użyć
+    tools=[get_weather_information], #podajemy toole ktore chcemy zeby agent uzyl
 )
 
 # Uruchamiamy agenta:
-def main():
-    Runner.run(agent, "Kraków") #uruchamianie agenta z gotowym user promptem (Krakow)
-
+async def main():
+    result = await Runner.run(agent, "Cracow") #uruchamianie agenta z gotowym user promptem (Krakow)
+    print(result.final_output)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main()) #robimy asyncio ze wzgledu na async i await w funkcji main
